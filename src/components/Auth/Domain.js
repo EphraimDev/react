59503a5c32d74c0fea7935cf876436ffa692e623
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2018-12-21 21:01:54 +0100

    Change domain to heroku

--- src/components/Auth/Domain.js
+++ src/components/Auth/Domain.js
@@ -1,2 +1,2 @@
-//export const domain = 'https://ephaig-web.herokuapp.com/api/v1'
-export const domain = 'http://localhost:5000/api/v1'
+export const domain = 'https://ephaig-web.herokuapp.com/api/v1'
+//export const domain = 'http://localhost:5000/api/v1'
